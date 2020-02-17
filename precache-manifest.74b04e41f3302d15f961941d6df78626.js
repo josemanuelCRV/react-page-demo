95a231f106c6c5f539ad5433ff4695494b2482f3
Jose Manuel Calvo <josema.calvo@servexternos.gruposantander.com>
2020-02-17 02:12:53 +0100

    Updates

---
+++ precache-manifest.74b04e41f3302d15f961941d6df78626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756bd71be26a9d0f3fc79b6fd4d48bb7",
+    "url": "/react-page-demo/index.html"
+  },
+  {
+    "revision": "0b202b3b57cfc5a214fc",
+    "url": "/react-page-demo/static/css/main.5c049345.chunk.css"
+  },
+  {
+    "revision": "7108686bd3f9ea9b1591",
+    "url": "/react-page-demo/static/js/2.e6158073.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-page-demo/static/js/2.e6158073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b202b3b57cfc5a214fc",
+    "url": "/react-page-demo/static/js/main.b10dd73e.chunk.js"
+  },
+  {
+    "revision": "21ad3be6b2b04189e4c4",
+    "url": "/react-page-demo/static/js/runtime-main.e216d0e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-page-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
